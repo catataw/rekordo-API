a64a84383a79bdbf22eb68766e6463a348047a40
luis <luis@fraski-laptop>
2016-01-22 17:26:06 +0100

    added database schema

---
+++ database/model/hash.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * rekordo.io
+ * (c) 2015-2016 luisan00
+ * This application is under a (MIT) license.
+ * See LICENSE file for more information.
+ * 
+ * Hash Schema.
+ */
+var mongoose = require('mongoose');
+
+var hashSchema = new mongoose.Schema({
+	hash : { type : String },
+	dateOrder : { type : Date },
+	keyPair : {
+		pubKey : { type : String },
+		prvKey : { type : String }
+	},
+	amount : { type : Number, default: 500000 },
+	amountPaid : { type : Number, default: 0 },
+	datePaid : { type : Date, default: null },
+	status : { type : Number, default: 0 },
+	block : { type : String, default: null },
+	txid : { type : String, default: null },
+	error: { type : String, default: null }
+});
+exports.hashModel = mongoose.model('hash', hashSchema);
