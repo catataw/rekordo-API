a34f3736399b7db1664ee330650692dda1458e52
luisan00 <luisan00@hotmail.com>
2016-02-18 14:08:21 +0100

    Update hash.js

--- database/model/hash.js
+++ database/model/hash.js
@@ -12,11 +12,8 @@ var mongoose = require('mongoose');
 var hashSchema = new mongoose.Schema({
 	hash : { type : String },
 	dateOrder : { type : Date },
-	keyPair : {
-		pubKey : { type : String },
-		prvKey : { type : String }
-	},
-	amount : { type : Number, default: 500000 },
+	prvKey : { type : String },
+	amount : { type : Number, default: 300000 },
 	amountPaid : { type : Number, default: 0 },
 	datePaid : { type : Date, default: null },
 	status : { type : Number, default: 0 },
@@ -24,4 +21,5 @@ var hashSchema = new mongoose.Schema({
 	txid : { type : String, default: null },
 	error: { type : String, default: null }
 });
+
 exports.hashModel = mongoose.model('hash', hashSchema);
